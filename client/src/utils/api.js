cf1eddcf3f22dd1f1c5cde2ab4142fb2e81c8f07
vinushinde2525 <vinushinde2525@gmail.com>
2026-06-21 06:13:54 +0530

    update cors production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,17 +1,160 @@
-import axios from 'axios';
+// src/services/api.js
 
+import axios from "axios";
+
+
+// Railway backend URL from .env
 const api = axios.create({
-  baseURL: '/api',
-  timeout: 8000,
+
+    baseURL:
+        import.meta.env.VITE_API_URL + "/api",
+
+    timeout: 8000,
+
+    headers: {
+        "Content-Type": "application/json",
+    },
+
 });
 
-export const getDestinations = (params = {}) => api.get('/destinations', { params });
-export const getDestination  = (id)       => api.get(`/destinations/${id}`);
-export const getPackages     = (params = {}) => api.get('/packages', { params });
-export const getPackage      = (id)       => api.get(`/packages/${id}`);
-export const getTestimonials = ()         => api.get('/testimonials');
-export const searchTours     = (data)     => api.post('/search', data);
-export const subscribeNewsletter = (email) => api.post('/newsletter', { email });
-export const submitContact   = (data)     => api.post('/contact', data);
+
+
+
+// Response interceptor
+api.interceptors.response.use(
+
+    (response) => response.data,
+
+
+    (error) => {
+
+        const message =
+            error.response?.data?.message ||
+            "Something went wrong. Please try again.";
+
+
+        return Promise.reject(
+            new Error(message)
+        );
+
+    }
+
+);
+
+
+
+
+
+
+
+
+export const getDestinations = (params = {}) =>
+
+    api.get(
+        "/destinations",
+        {
+            params
+        }
+    );
+
+
+
+export const getDestination = (id) =>
+
+    api.get(
+        `/destinations/${id}`
+    );
+
+
+
+
+
+
+
+
+
+export const getPackages = (params = {}) =>
+
+    api.get(
+        "/packages",
+        {
+            params
+        }
+    );
+
+
+
+export const getPackage = (id) =>
+
+    api.get(
+        `/packages/${id}`
+    );
+
+
+
+
+
+
+
+
+
+export const getTestimonials = () =>
+
+    api.get(
+        "/testimonials"
+    );
+
+
+
+
+
+
+
+
+
+
+export const searchTours = (data) =>
+
+    api.post(
+        "/search",
+        data
+    );
+
+
+
+
+
+
+
+
+
+
+export const subscribeNewsletter = (email) =>
+
+    api.post(
+        "/newsletter",
+        {
+            email
+        }
+    );
+
+
+
+
+
+
+
+
+
+export const submitContact = (data) =>
+
+    api.post(
+        "/contact",
+        data
+    );
+
+
+
+
 
 export default api;
